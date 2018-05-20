8cb0ba08e14f36033cb40fcbbbb0815f8ed68eb7
Kataev Vitaliy <vita@sshel.ru>
2018-05-20 23:26:06 +0300

    Add Post model

--- core/models.py
+++ core/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.utils.datetime_safe import datetime
+from django.template.defaultfilters import slugify
+from django.utils.translation import ugettext as _
 
-# Create your models here.
+
+# Post model
+class Post(models.Model):
+    title = models.CharField(max_length=255, null=False, blank=False, verbose_name=_('Заголовок'))
+    body = models.TextField(null=False, blank=False, verbose_name=_('Тест поста'))
+    created_at = models.DateTimeField(null=False, blank=False, verbose_name=_('Создано в'))
+    published_at = models.DateTimeField(null=False, blank=False, verbose_name=_('Опубликовано в'))
+    url_slug = models.CharField(max_length=150, null=False, blank=False, verbose_name=_('URL'), unique=True)
+    is_published = models.BooleanField(default=False, verbose_name=_('Опубликовано?'))
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+
+        if not self.published_at and self.is_published:
+            self.published_at = datetime.now()
+        if not self.pk:
+            self.created_at = datetime.now()
+
+        super(Post, self).save(*args, **kwargs)
+
+    class Meta:
+        db_table = 'posts'
+        verbose_name = _('Пост')
+        verbose_name_plural = _('Посты')
+        ordering = ('-published_at',)
